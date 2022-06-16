c891ce980ea38d00839d30d5bf265ed9b2e7f438
yangwon-park <ywonp94@gmail.com>
2022-06-16 17:20:32 +0900

    00. 프로젝트 생성

---
+++ src/main/java/ywphsm/ourneighbor/OurneighborApplication.java
@@ -0,0 +1,13 @@
+package ywphsm.ourneighbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurneighborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurneighborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ywphsm/ourneighbor/OurneighborApplicationTests.java
@@ -0,0 +1,13 @@
+package ywphsm.ourneighbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurneighborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

55de3fb1f90508ad8b9747e5449d93d35e4f5168
yangwon-park <97505799+yangwon-park@users.noreply.github.com>
2022-08-17 12:58:34 +0900

    메뉴 등록 폼 연동

--- src/main/java/ywphsm/ourneighbor/controller/MenuController.java
+++ src/main/java/ywphsm/ourneighbor/controller/MenuController.java
@@ -2,11 +2,12 @@ package ywphsm.ourneighbor.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.maven.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ywphsm.ourneighbor.domain.dto.MenuAddDTO;
 import ywphsm.ourneighbor.domain.store.Store;
 import ywphsm.ourneighbor.service.StoreService;
 
@@ -20,7 +21,9 @@ public class MenuController {
 
     @GetMapping("/addMenu/{storeId}")
     public String addMenu(@PathVariable Long storeId, Model model) {
-
+        MenuAddDTO dto = new MenuAddDTO();
+        dto.setStoreId(storeId);
+        model.addAttribute("menu", dto);
         return "menu/add_form";
     }
 }
--- src/main/java/ywphsm/ourneighbor/domain/dto/MenuAddDTO.java
+++ src/main/java/ywphsm/ourneighbor/domain/dto/MenuAddDTO.java
@@ -4,4 +4,10 @@ import lombok.Data;
 
 @Data
 public class MenuAddDTO {
+
+    private Long menuId;
+    private String name;
+    private Integer price;
+
+    private Long storeId;
 }

118caa3cd9b991f0dd390a415068ce56a3133f97
hansungmoon <arnold1998@naver.com>
2022-08-22 23:07:02 +0900

    수정 컨트롤러 테스트 마무리, 전화번호 수정로직 오류 해결

--- src/test/java/ywphsm/ourneighbor/controller/EditControllerTest.java
+++ src/test/java/ywphsm/ourneighbor/controller/EditControllerTest.java
@@ -1,8 +1,10 @@
 package ywphsm.ourneighbor.controller;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -10,24 +12,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import ywphsm.ourneighbor.controller.form.PhoneCertifiedForm;
 import ywphsm.ourneighbor.domain.member.Member;
 import ywphsm.ourneighbor.repository.member.MemberRepository;
 import ywphsm.ourneighbor.service.MemberService;
+import ywphsm.ourneighbor.service.email.EmailService;
 import ywphsm.ourneighbor.service.login.LoginService;
 import ywphsm.ourneighbor.service.login.SessionConst;
 
-import javax.servlet.http.HttpSession;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @Rollback
 public class EditControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    LoginService loginService;
 
     @LocalServerPort
     private int port;
@@ -37,12 +53,6 @@ public class EditControllerTest {
 
     @Autowired
     private MemberRepository memberRepository;
-
-    @Autowired
-    private MemberService memberService;
-
-    @Autowired
-    private LoginService loginService;
 
     @Test
     public void 닉네임_수정() throws Exception {
@@ -58,7 +68,6 @@ public class EditControllerTest {
         String url = "http://localhost:" + port + "/member_edit";
         ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, parameters, String.class);
 
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus);
         Member findMember = memberRepository.findById(loginMember.getId()).orElse(null);
         assertThat(findMember.getNickname()).isEqualTo("바뀐_닉네임");
     }
@@ -67,24 +76,106 @@ public class EditControllerTest {
     public void 비밀번호_수정() throws Exception {
         //given
         Member loginMember = loginService.login("ailey", "12341234");
-        HttpSession session = new MockHttpSession();
+        MockHttpSession session = new MockHttpSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
-        parameters.add("beforePassword", "12341234");
-        parameters.add("afterPassword", "Arnold!(97");
-        parameters.add("passwordCheck", "Arnold!(97");
+        mvc.perform(
+                        MockMvcRequestBuilders.post("/member_edit/password_edit")
+                                .param("beforePassword", "12341234")
+                                .param("afterPassword", "Arnold!(97")
+                                .param("passwordCheck", "Arnold!(97")
+                                .session(session))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
 
-        String url = "http://localhost:" + port + "/member_edit/password_edit";
 
-        //when
-        ResponseEntity<Object> responseEntity = restTemplate.postForEntity(url, parameters, Object.class);
-
-        //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        Member findMember = memberRepository.findById(loginMember.getId()).orElse(null);
+        Member findMember = memberService.findOne(loginMember.getId());
         assertThat(memberService.passwordCheck(findMember.getPassword(), "Arnold!(97")).isTrue();
     }
 
+    @Test
+    public void 이메일인증_Get() throws Exception {
+        //given
+
+        Member loginMember2 = loginService.login("ailey", "12341234");
+        MockHttpSession session2 = new MockHttpSession();
+        session2.setAttribute(SessionConst.LOGIN_MEMBER, loginMember2);
+
+        Member loginMember = loginService.login("ailey", "12341234");
+        loginMember.emailConfirmSuccess();
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        //이메일 인증이 false
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/member_edit/confirm_email")
+                                .session(session2))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("edit/EmailConfirmForm"));
+
+        //이메일 인증이 true
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/member_edit/confirm_email")
+                                .session(session))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("edit/alreadyEmailConfirm"));
+    }
+
+    @Test
+    public void 이메일인증_Post() throws Exception {
+        //given
+        Member loginMember = loginService.login("ailey", "12341234");
+        loginMember.emailConfirmSuccess();
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        mvc.perform(
+                        MockMvcRequestBuilders.post("/member_edit/confirm_email")
+                                .session(session)
+                                .param("email", loginMember.getEmail()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("signUp/confirmEmail"));
+    }
+
+    @Test
+    public void 전화번호인증_Post() throws Exception {
+        //given
+        Member loginMember = loginService.login("ailey", "12341234");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        PhoneCertifiedForm phone = new PhoneCertifiedForm();
+        phone.setPhoneNumber("01038352376");
+        phone.setCertifiedNumber("123456");
+        session.setAttribute(SessionConst.PHONE_CERTIFIED, phone);
+
+        //핸드폰인증이 성공시
+        mvc.perform(
+                        MockMvcRequestBuilders.post("/member_edit/phoneCertified")
+                                .session(session)
+                                .param("phoneNumber", "01038352376")
+                                .param("certifiedNumber", "123456"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/logout"));
+
+        //then
+        assertThat(memberService.findOne(loginMember.getId()).getPhoneNumber()).isEqualTo("01038352376");
+    }
+
+    @Test
+    public void 회원탈퇴() throws Exception {
+        //given
+        Member loginMember = loginService.login("ailey", "12341234");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        mvc.perform(
+                        MockMvcRequestBuilders.get("/member_edit/withdrawal")
+                                .session(session))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/logout"));
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            memberService.findOne(loginMember.getId());
+        });
+    }
 }

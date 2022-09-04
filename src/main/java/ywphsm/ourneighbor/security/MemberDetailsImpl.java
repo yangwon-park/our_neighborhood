a5747c0ca079bd8ac5413317318989a325491b0a
hansungmoon <arnold1998@naver.com>
2022-09-04 20:12:54 +0900

    잘못된 푸시를 수정

---
+++ src/main/java/ywphsm/ourneighbor/security/MemberDetailsImpl.java
@@ -0,0 +1,62 @@
+package ywphsm.ourneighbor.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ywphsm.ourneighbor.domain.member.Member;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class MemberDetailsImpl implements UserDetails {
+
+    private Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return String.valueOf(member.getMemberRole());
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    //계정이 만료되지 않았는지 리턴 (true: 만료안됨)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //계정이 잠겨있는지 않았는지 리턴. (true:잠기지 않음)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //비밀번호가 마료되지 않았는지 리턴한다. (true:만료안됨)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    //계정이 활성화(사용가능)인지 리턴 (true:활성화)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+

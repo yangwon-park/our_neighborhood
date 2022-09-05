16aee3f0cfd994761d65fb114e224561f663150c
yangwon-park <97505799+yangwon-park@users.noreply.github.com>
2022-09-05 21:09:55 +0900

    기상청 단기 예보, 미세먼지 API 연동 (정적)

--- src/main/java/ywphsm/ourneighbor/api/MapSearchController.java
+++ src/main/java/ywphsm/ourneighbor/api/MapSearchController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ywphsm.ourneighbor.domain.dto.CategoryDTO;
 import ywphsm.ourneighbor.domain.store.Store;
 import ywphsm.ourneighbor.repository.store.dto.SimpleSearchStoreDTO;
 import ywphsm.ourneighbor.service.CategoryService;
@@ -19,7 +18,7 @@ public class MapSearchController {
 
     private final StoreService storeService;
     private final CategoryService categoryService;
-    
+
     // 메뉴 리스트는 불러오지 않음
     // 단순 조회이므로 fetch 조인으로 최적화
     @GetMapping(value = "/searchStores", produces = "application/json;charset=utf-8")
---
+++ src/main/java/ywphsm/ourneighbor/api/dto/WeatherDTO.java
@@ -0,0 +1,14 @@
+package ywphsm.ourneighbor.api.dto;
+
+import lombok.Data;
+
+@Data
+public class WeatherDTO {
+
+    private String POP;         // 강수 확률  (%)
+    private String PTY;         // 강수 형태  (0: 없음, 1: 비, 2: 비/눈, 3: 눈, 4: 소나기)
+    private String SKY;         // 하늘 상태  (1: 맑음, 3: 구름 많음, 4: 흐림)
+    private String TMP;         // 1시간 기온 (섭씨)
+
+    private String pm10Value;   // 미세먼지 농도
+}

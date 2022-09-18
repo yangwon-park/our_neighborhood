e9fbcd3f4d4e5f803506560f7f5a53eb5fb3853f
yangwon-park <97505799+yangwon-park@users.noreply.github.com>
2022-09-18 21:36:28 +0900

    카테고리 중복 체크 구현 (프론트, 백(axios)), 카테고리 삭제 기능 구현

--- src/main/java/ywphsm/ourneighbor/api/CategoryApiController.java
+++ src/main/java/ywphsm/ourneighbor/api/CategoryApiController.java
@@ -2,11 +2,11 @@ package ywphsm.ourneighbor.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ywphsm.ourneighbor.domain.Category;
 import ywphsm.ourneighbor.domain.dto.CategoryDTO;
 import ywphsm.ourneighbor.service.CategoryService;
 
@@ -30,9 +30,22 @@ public class CategoryApiController {
         return categoryService.findAllCategoriesHier().get(0);
     }
 
+    @GetMapping("/categoryCheck")
+    public ResponseEntity<Boolean> checkCategoryDuplicate(String name, Long parentId) {
+        Category parent = categoryService.findById(parentId);
+        return ResponseEntity.ok(categoryService.checkCategoryDuplicate(name, parent));
+    }
+
+
     @PostMapping("/category/add")
     public Long save(@Validated CategoryDTO dto) {
-
         return categoryService.saveCategory(dto);
+    }
+
+    @DeleteMapping("/category/delete/{categoryId}")
+    public Long delete(@PathVariable Long categoryId) {
+        categoryService.deleteCategory(categoryId);
+
+        return categoryId;
     }
 }

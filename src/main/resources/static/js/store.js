412a406bb55891500827bbed8f4a0a555b421a33
yangwon-park <ywonp94@gmail.com>
2022-09-26 20:50:30 +0900

    매장 등록 Validation 구현

--- src/main/resources/static/js/store.js
+++ src/main/resources/static/js/store.js
@@ -1,3 +1,5 @@
+import validation from "./validation.js";
+
 var main = {
     init: function () {
         var _this = this;
@@ -28,35 +30,106 @@ var main = {
     },
 
     save: function () {
-        const storeForm = document.getElementById('store-add-form');
 
+        // input 태그
+        const els = {
+            name: document.getElementById("name"),
+            zipcode: document.getElementById("zipcode"),
+            roadAddr: document.getElementById("roadAddr"),
+            numberAddr: document.getElementById("numberAddr"),
+            openingTime: document.getElementById("openingTime"),
+            closingTime: document.getElementById("closingTime")
+        }
+
+        // input 아래의 validation을 담을 div 태그
+        const valids = {
+            nameValid: document.getElementById("store-name-valid"),
+            zipcodeValid: document.getElementById("store-zipcode-valid"),
+            roadAddrValid: document.getElementById("store-roadAddr-valid"),
+            numberAddrValid: document.getElementById("store-numberAddr-valid"),
+            openingTimeValid: document.getElementById("store-openingTime-valid"),
+            closingTimeValid: document.getElementById("store-closingTime-valid")
+        }
+
+
+        const cateValid = document.getElementById("store-category-valid");
+
+        const mainCateVal = document.getElementById("main-cate").options
+                        [document.getElementById("main-cate").selectedIndex].value;
+        const midCateVal = document.getElementById("mid-cate").options
+                        [document.getElementById("mid-cate").selectedIndex].value;
+        const subCateVal = document.getElementById("sub-cate").options
+                        [document.getElementById("sub-cate").selectedIndex].value;
+
+        const storeForm = document.getElementById('store-add-form');
         const formData = new FormData(storeForm);
 
-        axios({
-            method: "post",
-            url: "/store",
-            data: formData
-        }).then((resp) => {
-            alert('매장 등록이 완료됐습니다.');
-            window.location.href = "/";
-        }).catch((error) => {
-            console.error(error);
-        })
+        for (const el in els) {
+            els[el].classList.remove("valid-custom");
+        }
+
+        for (const v in valids) {
+            validation.removeValidation(valids[v]);
+        }
+
+        validation.removeValidation(cateValid);
+
+        this.categoryLayerEl.main.classList.remove("input-error-border");
+        this.categoryLayerEl.mid.classList.remove("input-error-border");
+        this.categoryLayerEl.sub.classList.remove("input-error-border");
+
+        if (els["name"].value !== '' && els["zipcode"].value !== ''
+            && els["roadAddr"].value !== '' && els["numberAddr"].value !== ''
+            && els["openingTime"].value !== '' && els["closingTime"].value !== ''
+            && mainCateVal !== '' && midCateVal !== '' && subCateVal !== '') {
+
+            axios({
+                method: "post",
+                url: "/store",
+                data: formData
+            }).then((resp) => {
+                alert("매장 등록이 완료됐습니다.");
+                window.location.href = "/";
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+
+        for (const el in els) {
+            if (els[el].value === '') {
+                els[el].classList.add("valid-custom");
+                validation.addValidation(valids[el + "Valid"], "값을 입력해주세요.");
+            }
+        }
+
+        if (subCateVal === '') {
+            this.categoryLayerEl.sub.classList.add("input-error-border");
+            validation.addValidation(cateValid, "소분류를 선택해주세요.");
+        }
+        if (midCateVal === '') {
+            this.categoryLayerEl.mid.classList.add("input-error-border");
+            validation.addValidation(cateValid, "중분류를 선택해주세요.");
+        }
+
+        if (mainCateVal === '') {
+            this.categoryLayerEl.main.classList.add("input-error-border");
+            validation.addValidation(cateValid, "대분류를 선택해주세요.");
+        }
     },
 
     update: function () {
         const storeForm = document.getElementById('store-edit-form');
-        const storeId = document.getElementById('storeId').value;
+        const storeIdVal = document.getElementById('storeId').value;
 
         const formData = new FormData(storeForm);
 
         axios({
             method: "put",
-            url: "/store/" + storeId,
+            url: "/store/" + storeIdVal,
             data: formData
         }).then((resp) => {
             alert('매장 정보 수정이 완료됐습니다.');
-            window.location.href = "/store/" + storeId;
+            window.location.href = "/store/" + storeIdVal;
         }).catch((error) => {
             console.error(error);
         })
@@ -96,7 +169,6 @@ var main = {
     },
 
     getMainCategories: function (rootChildren) {
-
         // 대분류는 미리 저장함
         for (const rc of rootChildren) {
             let mainOption = document.createElement("option");
@@ -106,7 +178,6 @@ var main = {
 
             this.mainChildren.push(rc.children)
         }
-
     },
 
     changeMainCategories: function (mainChildrenParam) {

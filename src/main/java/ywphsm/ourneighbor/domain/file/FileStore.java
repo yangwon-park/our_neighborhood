9161effbdde8be9c862cd03dfa192fe4d9c4f25b
yangwon-park <97505799+yangwon-park@users.noreply.github.com>
2022-10-12 22:41:42 +0900

    active profile에 맞게 createFile path 수정

--- src/main/java/ywphsm/ourneighbor/domain/file/FileStore.java
+++ src/main/java/ywphsm/ourneighbor/domain/file/FileStore.java
@@ -3,6 +3,7 @@ package ywphsm.ourneighbor.domain.file;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 

3281bd83dda1a4c0dcb5e908a64446d3a8757e73
yangwon-park <97505799+yangwon-park@users.noreply.github.com>
2022-10-27 18:25:01 +0900

    중복 코드 리팩토링

--- src/main/java/ywphsm/ourneighbor/controller/HomeController.java
+++ src/main/java/ywphsm/ourneighbor/controller/HomeController.java
@@ -20,39 +20,11 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    private final StoreService storeService;
-
     private final CategoryService categoryService;
 
     @GetMapping("/")
-    public String index(Model model,
-                        @CookieValue(value = "lat", required = false) String lat,
-                        @CookieValue(value = "lon", required = false) String lon) {
-
+    public String index(Model model) {
         List<CategoryDTO.Simple> rootCategoryList = categoryService.findByDepth(1L);
-
-
-        if (lat != null && lon != null) {
-            // 최선의 코드인 것 같지가 않음
-            // 더 좋은 방법이 생각나거나 알게 된다면 추후에 리팩토링하자
-            List<String> restaurantImages = storeService.getTop5ImageByCategories((rootCategoryList.get(0).getCategoryId().toString()),
-                    Double.parseDouble(lat), Double.parseDouble(lon));
-
-            List<String> cafeImages = storeService.getTop5ImageByCategories((rootCategoryList.get(1).getCategoryId().toString()),
-                    Double.parseDouble(lat), Double.parseDouble(lon));
-
-            List<String> barImages = storeService.getTop5ImageByCategories((rootCategoryList.get(2).getCategoryId().toString()),
-                    Double.parseDouble(lat), Double.parseDouble(lon));
-
-            List<String> leisureImages = storeService.getTop5ImageByCategories((rootCategoryList.get(3).getCategoryId().toString()),
-                    Double.parseDouble(lat), Double.parseDouble(lon));
-
-
-            model.addAttribute("restaurantImages", restaurantImages);
-            model.addAttribute("cafeImages", cafeImages);
-            model.addAttribute("barImages", barImages);
-            model.addAttribute("leisureImages", leisureImages);
-        }
 
         model.addAttribute("rootCategoryList", rootCategoryList);
 

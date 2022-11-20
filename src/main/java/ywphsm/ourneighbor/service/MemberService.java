767c2890fecd53a43caea821ae367780ffbb8b7e
hansungmoon <arnold1998@naver.com>
2022-11-20 15:26:48 +0900

    [Refactor] "footer 디자인 변경"
    footer 변경

--- src/main/java/ywphsm/ourneighbor/service/MemberService.java
+++ src/main/java/ywphsm/ourneighbor/service/MemberService.java
@@ -194,9 +194,9 @@ public class MemberService {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(email);   //보낼 이메일주소 추가
         mailMessage.setSubject("비밀번호 찾기 Our neighborhood");  //제목
-        mailMessage.setText("임시 비밀번호를 발급했습니다.");
-        mailMessage.setText("임시 비밀번호는 " + temporaryPassword + "입니다.");
-        mailMessage.setText("로그인후 마이페이지에서 비밀번호를 변경해 주세요.");
+        mailMessage.setText("임시 비밀번호를 발급했습니다.\n" +
+            "임시 비밀번호는 " + temporaryPassword + "입니다.\n" +
+            "로그인후 마이페이지에서 비밀번호를 변경해 주세요.");
         emailService.sendEmail(mailMessage);
         log.info("비밀번호 찾기 이메일 발송 완료 임시비밀번호={}", temporaryPassword);
 
